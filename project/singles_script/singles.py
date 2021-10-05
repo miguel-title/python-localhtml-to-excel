de68df515fe433b2237ba4a6f75adcd88cf46f98
xiao-title <tomas198606289@gmail.com>
2021-10-06 02:08:09 +0900

    adjust cell width

--- project/singles_script/singles.py
+++ project/singles_script/singles.py
@@ -19,6 +19,13 @@ import time
 import random
 
 
+import xlsxwriter
+from xlsxwriter.worksheet import (
+    Worksheet, cell_number_tuple, cell_string_tuple)
+
+from typing import Optional
+
+
 #Import the os in order to get the current directory
 import os
 ROOT_PATH = os.path.realpath(os.path.dirname(os.path.abspath(__file__)))
@@ -150,17 +157,64 @@ class SinglesApp():
 
     def writeDatatoExcel(self, result):
         outputfilename = self.outputpath + '/' + datetime.now().strftime("%Y%m%d%H%M%S") + '.xlsx'
-        import xlsxwriter
 
         workbook = xlsxwriter.Workbook(outputfilename)
         worksheet = workbook.add_worksheet()
-
+        
+        header_cell_format = workbook.add_format({'bold': True, 'font_color': 'red'})
+        content_cell_format = workbook.add_format()
+        content_cell_format.set_bottom()
+        content_cell_format.set_left()
+        content_cell_format.set_right()
         for row_num, row_data in enumerate(result):
-            for col_num, col_data in enumerate(row_data):
-                worksheet.write(row_num, col_num, col_data)
+                for col_num, col_data in enumerate(row_data):
+                    if row_num == 0:
+                        worksheet.write(row_num, col_num, col_data, header_cell_format)
+                    else:
+                        worksheet.write(row_num, col_num, col_data, content_cell_format)
 
+        for index in range(10):
+            self.set_column_autowidth(worksheet, index)
+        
         workbook.close()
 
+    def get_column_width(self, worksheet: Worksheet, column: int) -> Optional[int]:
+        """Get the max column width in a `Worksheet` column."""
+        strings = getattr(worksheet, '_ts_all_strings', None)
+        if strings is None:
+            strings = worksheet._ts_all_strings = sorted(
+                worksheet.str_table.string_table,
+                key=worksheet.str_table.string_table.__getitem__)
+        lengths = set()
+        for row_id, colums_dict in worksheet.table.items():  # type: int, dict
+            data = colums_dict.get(column)
+            if not data:
+                continue
+            if type(data) is cell_string_tuple:
+                iter_length = len(strings[data.string])
+                if not iter_length:
+                    continue
+                lengths.add(iter_length)
+                continue
+            if type(data) is cell_number_tuple:
+                iter_length = len(str(data.number))
+                if not iter_length:
+                    continue
+                lengths.add(iter_length)
+        if not lengths:
+            return None
+        return max(lengths)
+
+    def set_column_autowidth(self, worksheet: Worksheet, column: int):
+        """
+        Set the width automatically on a column in the `Worksheet`.
+        !!! Make sure you run this function AFTER having all cells filled in
+        the worksheet!
+        """
+        maxwidth = self.get_column_width(worksheet=worksheet, column=column)
+        if maxwidth is None:
+            return
+        worksheet.set_column(first_col=column, last_col=column, width=maxwidth)
 
 if __name__ == "__main__":
     app = SinglesApp()
